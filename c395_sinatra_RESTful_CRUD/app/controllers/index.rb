0553a82bb0cf8e5346b6ff0f5af5886f441a66bb
amgando <sherif@amgando.org>
2013-11-04 12:46:38 -0800

    objective 1: user can create a new note

--- c395_sinatra_RESTful_CRUD/app/controllers/index.rb
+++ c395_sinatra_RESTful_CRUD/app/controllers/index.rb
@@ -2,3 +2,23 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+# ######################################################
+# # SHOW NOTES
+# ######################################################
+
+get '/notes' do
+end
+
+######################################################
+# CREATE NOTE
+######################################################
+
+get '/notes/new' do
+  erb :new_note
+end
+
+post '/notes' do
+  @note = Note.create!(params[:note])
+  erb :confirm_new_note
+end
